a9aa9654cc2c378bbed6a8d18b304a464c2692c2
rayhong <rayhong118@gmail.com>
2026-04-16 22:22:19 -0700

    CH1 complete

--- cli/keyword_search_cli.py
+++ cli/keyword_search_cli.py
@@ -1,6 +1,8 @@
 import argparse
 import json
 import string
+import io
+from nltk.stem import PorterStemmer
 
 def main() -> None:
     parser = argparse.ArgumentParser(description="Keyword Search CLI")
@@ -19,7 +21,7 @@ def main() -> None:
         case _:
             parser.print_help()
 
-def keywordSearch(keyword): 
+def keywordSearch(query): 
     with open('./data/movies.json', 'r') as f:
         dictionary = json.load(f)
 
@@ -27,21 +29,51 @@ def keywordSearch(keyword):
 
     results = []
 
+    tokenizedQuery = tokenizeStrings(query)
+
+    print(tokenizedQuery)
+
     # translation table to remove punctuation
     transTable = str.maketrans('', '', string.punctuation)
 
     for movie in movies:
+
         title = movie["title"].translate(transTable)
-        if keyword.lower() in title.lower():
-            results.append(movie)
-            if len(results) == 5:
+        tokenizedTitle = tokenizeStrings(title)
+
+        for query_word in tokenizedQuery:
+            if any(query_word in title_word for title_word in tokenizedTitle):
+                results.append(movie)
                 break
+
 
     for i, result in enumerate(results):
         print(f'{i+1}. {result["title"]}')
     
     pass
 
+def tokenizeStrings (input):
+    result = []
+
+    # stopwords
+    stopwords = getStopWords()
+
+    # stemming
+    stemmer = PorterStemmer()
+
+    inputList = input.lower().split(" ")
+    for inputWord in inputList:
+        if inputWord != "" and inputWord not in stopwords:
+            result.append(stemmer.stem(inputWord))
+
+    return result
+
+def getStopWords():
+    with open('./data/stopwords.txt', 'r') as f:
+        content = f.read()
+        return content.splitlines()
+        
 
 if __name__ == "__main__":
     main()
+
