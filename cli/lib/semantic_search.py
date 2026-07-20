c745d06f755604a7dbd6998dac25aed3bef50f4b
rayhong <rayhong118@gmail.com>
2026-07-20 16:38:42 -0700

    ch4L10

--- cli/lib/semantic_search.py
+++ cli/lib/semantic_search.py
@@ -9,6 +9,13 @@ class SemanticSearch:
     def search(self, query: str) -> list[tuple[str, float]]:
         pass
 
+    def generate_embedding(self, text: str):
+        if len(text) == 0 or text.strip() == "":
+            raise ValueError("Text cannot be empty")
+
+        embedding = self.model.encode([text])[0]
+        return embedding
+
 
 def verify_model():
     print("Verifying model...")
@@ -16,3 +23,10 @@ def verify_model():
     search_instance = SemanticSearch()
     print(f'Model loaded: {search_instance.model}')
     print(f'Max sequence length: {search_instance.model.max_seq_length}')
+
+def embed_text(text:str):
+    semantic_search = SemanticSearch()
+    embedding = semantic_search.generate_embedding(text)
+    print(f"Text: {text}")
+    print(f"First 3 dimensions: {embedding[:3]}")
+    print(f"Dimensions: {embedding.shape[0]}")
--- cli/semantic_search_cli.py
+++ cli/semantic_search_cli.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
 import argparse
-from lib.semantic_search import verify_model
+from lib.semantic_search import verify_model, embed_text
 
 def main():
     parser = argparse.ArgumentParser(description="Semantic Search CLI")
     subparsers = parser.add_subparsers(dest="command", help="Available commands")
-    subparsers.add_parser("verify", help="Verify the model is loaded correctly")
+    
+    verify_parser = subparsers.add_parser("verify", help="Verify the model is loaded correctly")
+
+    embed_parser = subparsers.add_parser("embed_text", help="Embed text")
+    embed_parser.add_argument("text", type=str, help="Text to embed")
     
     args = parser.parse_args()
 
     match args.command:
         case "verify":
             verify_model()
+        case "embed_text":
+            embed_text(args.text)
         case _:
             parser.print_help()
 
 if __name__ == "__main__":
     main()
+
